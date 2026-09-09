81949199bb537499536fece82eb90dbf17088b6b
Satyanam1 <satya949496@gmail.com>
2026-09-09 13:01:29 +0530

    svg path

---
+++ day3/script.js
@@ -0,0 +1,29 @@
+var path = "M 10 100 Q 960 100 1990 100";
+var finalPath = "M 10 100 Q 960 100 1990 100";
+
+var string = document.querySelector("#string");
+
+string.addEventListener("mousemove", function (dets) {
+
+    path = `M 10 100 Q ${dets.x} ${dets.y} 1990 100`;
+
+    gsap.to("svg path", {
+        attr: {
+            d: path
+        },
+        duration: 0.3,
+        ease: "power3.out"
+    });
+});
+
+string.addEventListener("mouseleave", function () {
+
+    gsap.to("svg path", {
+        attr: {
+            d: finalPath
+        },
+        duration: 1.5,
+        ease: "elastic.out(1,0.2)"
+    });
+
+});
